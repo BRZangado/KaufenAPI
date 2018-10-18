67662384783133c30c7cf779263dc0b1cf75c238
Bruno Rodrigues <bruesmanbruesman@hotmail.com>
2018-10-17 21:36:56 -0300

    updated endpoint return type

--- src/main/java/com/example/kaufen/controllers/ProductsController.java
+++ src/main/java/com/example/kaufen/controllers/ProductsController.java
@@ -1,9 +1,11 @@
 package com.example.kaufen.controllers;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.kaufen.models.Product;
@@ -18,6 +20,11 @@ public class ProductsController{
 	@GetMapping("/products")
 	public List<Product> all(){
 		return pr.findAll();
+	}
+	
+	@GetMapping("/products/{id}")
+	public Optional<Product> get_by_id(@PathVariable Long id){
+		return pr.findById(id);
 	}
 	
 }
